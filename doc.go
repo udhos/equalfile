8044f1faf57852647e11aa7aaebbf761ed69dfdd
Everton Marques <everton.marques@gmail.com>
2016-12-10 01:59:12 -0200

    Doc.

---
+++ doc.go
@@ -0,0 +1,29 @@
+/*
+Package equalfile provides facilities for comparing files.
+
+Equalfile is similar to Python's filecmp:
+
+        import filecmp
+        if filecmp.cmp(filename1, filename2, shallow=False):
+
+Comparing only two files
+
+In single mode, equalfile compares files byte-by-byte.
+
+        import "github.com/udhos/equalfile"
+        // ...
+	equal, err := equalfile.CompareFile("file1", "file2") // compare using single mode
+
+Comparing multiple files
+
+In multiple mode, equalfile records files hashes in order to speedup repeated comparisons.
+You must provide the hashing function.
+
+        import "crypto/sha256"
+        import "github.com/udhos/equalfile"
+        // ...
+        equalfile.CompareMultiple(sha256.New(), true) // enable multiple mode
+	equal, err := equalfile.CompareFile("file1", "file2")
+
+*/
+package equalfile
